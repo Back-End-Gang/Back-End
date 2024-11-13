a4acdc8ba81fc3e3e9a26d9d5ca1e0f54b4cf5ea
Alexis <alexismhormazabal@gmail.com>
2024-11-13 15:27:29 -0300

    Creación de modelos

--- biblioteca_citas/citas/models.py
+++ biblioteca_citas/citas/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 # Create your models here.
 class Cita(models.Model):
     texto= models.TextField(max_length=250)
     autor= models.TextField(max_length=100)
-    fecha= models.TextField(max_length=100)
+    fecha= models.IntegerField(validators=[MaxValueValidator(1900),MinValueValidator(2024)])
     fuente= models.TextField(max_length=100)
 
 class Fuente(models.Model):
