ae55da516dbc96c21cb899efdc59372e701e0885
Alexis <alexismhormazabal@gmail.com>
2024-11-13 15:14:41 -0300

    Creacion de modelos

--- biblioteca_citas/citas/models.py
+++ biblioteca_citas/citas/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Cita(models.Model):
+    texto= models.TextField(max_length=250)
+    autor= models.TextField(max_length=100)
+    fecha= models.TextField(max_length=100)
+    fuente= models.TextField(max_length=100)
+
+class Fuente(models.Model):
+    Nombre= models.TextField(max_length=250)
+    Tipo= models.TextField(max_length=100)
