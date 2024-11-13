eaa0a9adc3b7b665d6175c80cdc3c97ce04729d6
Alexis <alexismhormazabal@gmail.com>
2024-11-13 15:37:05 -0300

    Modificacion models

--- biblioteca_citas/citas/models.py
+++ biblioteca_citas/citas/models.py
@@ -23,6 +23,3 @@ class Cita(models.Model):
     fecha= models.IntegerField(validators=[MaxValueValidator(1900),MinValueValidator(2024)])
     fuente= models.TextField(max_length=100)
 
-class Fuente(models.Model):
-    Nombre= models.TextField(max_length=250)
-    Tipo= models.TextField(max_length=100)
